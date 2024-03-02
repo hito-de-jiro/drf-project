ff418549939602ce32edf898f456a9d234a4fcb1
Hito <60074087+hito-de-jiro@users.noreply.github.com>
2024-03-02 16:31:49 +0200

    fetch: added first model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    item = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    weight = models.DecimalField(max_digits=10, decimal_places=3)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.item) + ": $" + str(self.price) + " weight: " + str(self.weight) + " kg"
+
+
--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from .views import index
 
+
+
 urlpatterns = [
     path('index/', index, name='index'),
 ]
