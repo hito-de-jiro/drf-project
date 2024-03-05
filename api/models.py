f5af8b0dc22164232621bd4cbdb7cda928a9063f
Hito <60074087+hito-de-jiro@users.noreply.github.com>
2024-03-05 10:44:46 +0200

    fetch: added two new models to models.py

--- api/models.py
+++ api/models.py
@@ -1,5 +1,20 @@
-from django.contrib.auth.models import User
 from django.db import models
+
+
+class Owner(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.URLField()
+    watched = models.BooleanField(default=False)
+    time_watched = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
 
 
 class Product(models.Model):
@@ -7,9 +22,8 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     weight = models.DecimalField(max_digits=10, decimal_places=3)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.item) + ": $" + str(self.price) + " weight: " + str(self.weight) + " kg"
-
-
