dd440800a1ec99296ad8436439abcaf5158e7790
Hito <60074087+hito-de-jiro@users.noreply.github.com>
2024-03-08 19:33:34 +0200

    fix(models): redesigned models

--- api/models.py
+++ api/models.py
@@ -1,30 +1,38 @@
 from django.db import models
 
 
-class User(models.Model):
-    name = models.CharField(max_length=255)
-    email = models.EmailField()
+class Owner(models.Model):
+    name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
 
 
 class Product(models.Model):
-    owner = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, unique=True)
     description = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.name)
 
 
 class Lesson(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, unique=True)
     url = models.URLField()
     duration = models.IntegerField()
     time_watched = models.DurationField(blank=True, null=True)
-    product = models.ManyToManyField(Product)
+    status_watched = models.BooleanField(default=False)
+    product = models.ManyToManyField(Product, related_name='lessons')
 
     def __str__(self):
         return str(self.title)
+
+
+class User(models.Model):
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField()
+    lessons = models.ManyToManyField(Lesson, related_name='watched_lessons', blank=True)
+
+    def __str__(self):
+        return self.username
