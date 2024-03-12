249662f2c4b02fe042c643fc108c19c9c0d09b99
Hito <60074087+hito-de-jiro@users.noreply.github.com>
2024-03-12 17:58:54 +0200

    feat(models): changed models

--- api/models.py
+++ api/models.py
@@ -2,15 +2,9 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class Owner(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-
-
 class Product(models.Model):
-    owner = models.ForeignKey(Owner, verbose_name='owner', on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, verbose_name='owner', on_delete=models.CASCADE)
     name = models.CharField(max_length=255, unique=True)
-    description = models.TextField()
 
     def __str__(self):
         return str(self.name)
@@ -20,19 +14,18 @@ class Lesson(models.Model):
     title = models.CharField(max_length=255)
     url = models.URLField()
     duration = models.IntegerField(default=0)
-    time_watched = models.DurationField(blank=True, null=True)
-    status_watched = models.BooleanField(default=False)
-    last_watched = models.DateTimeField(auto_now_add=True, blank=True, null=True)
-    product = models.ManyToManyField(Product, related_name='product_lessons')
+
+    product = models.ManyToManyField(Product, related_name='products_lessons')
 
     def __str__(self):
         return str(self.title)
 
 
-class Customer(models.Model):
-    username = models.CharField(max_length=255, unique=True)
-    email = models.EmailField()
-    product = models.ForeignKey(Product, verbose_name='product', blank=True, null=True, on_delete=models.PROTECT)
+class UserLesson(models.Model):
+    user = models.ForeignKey(User, verbose_name='user', on_delete=models.PROTECT)
+    lesson = models.ForeignKey(Lesson, verbose_name='lesson', on_delete=models.PROTECT)
 
-    def __str__(self):
-        return self.username
+    time_watched = models.DurationField(blank=True, null=True)
+    status_watched = models.BooleanField(default=False)
+
+    last_watched = models.DateTimeField(auto_now_add=True, blank=True, null=True)
