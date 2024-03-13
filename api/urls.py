0767d19cdce19c70a9925afae4e89386ff4a9a42
Hito <60074087+hito-de-jiro@users.noreply.github.com>
2024-03-13 15:12:18 +0200

    feat(view): added class UserLessonListAPIView and url

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,26 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
 from .views import (
     LessonAPIList,
     ProductAPIList,
-    UserLessonAPIList,
+    UserLessonListAPIView,
+    # UserLessonAPIList,
+    # UserLessonAPIDetail,
 )
 
+
+# lessons = UserLessonAPIDetail.as_view({'get': 'list'})
+# lesson = UserLessonAPIDetail.as_view({'get': 'retrieve'})
+#
+# router = DefaultRouter()
+# router.register(r'customer-lessons', UserLessonAPIDetail, basename='lesson')
+
 urlpatterns = [
-    path('user-lessons/', UserLessonAPIList.as_view()),
+    # path('customer-lessons/', include(router.urls)),
+    # path('user-lessons/', UserLessonAPIList.as_view()),
+
+    path('user-lessons/', UserLessonListAPIView.as_view()),
 
     path('products/', ProductAPIList.as_view()),
     path('lessons/', LessonAPIList.as_view()),
