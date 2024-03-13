67061dde1e928d604fa85ed09fa993a56e41ceca
Hito <60074087+hito-de-jiro@users.noreply.github.com>
2024-03-13 15:10:51 +0200

    refactor(serializers): refactor serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -4,28 +4,34 @@ from .models import Lesson, UserLesson, Product
 
 
 class LessonSerializer(serializers.ModelSerializer):
-    # product = ProductSerializer(many=True, read_only=True)
-    products_lessons = serializers.StringRelatedField(many=True, read_only=True)
-
     class Meta:
         model = Lesson
-        fields = ['title', 'url', 'duration', 'product', 'products_lessons']
+        fields = '__all__'
+
+        depth = 1
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    products_lessons = LessonSerializer(many=True, read_only=True)
+    products_lessons = serializers.StringRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Product
-        fields = ['name', 'products_lessons']
+        fields = ['owner_id', 'name', 'products_lessons']
         depth = 1
+
+
+# class UserLessonSerializer(serializers.ModelSerializer):
+#     # product_name = ProductSerializer(many=True, read_only=True)
+#     lessons = LessonSerializer(many=True)
+#
+#     class Meta:
+#         model = UserLesson
+#         # fields = '__all__'
+#         fields = ['lessons', 'title', 'time_watched', 'status_watched', ]
+#         # depth = 2
 
 
 class UserLessonSerializer(serializers.ModelSerializer):
-    product = ProductSerializer(read_only=True, many=True)
-
     class Meta:
         model = UserLesson
-        fields = ['lesson', 'time_watched', 'status_watched', 'product']
-        depth = 1
-        read_only_fields = ['lesson', 'time_watched', 'status_watched', ]
+        fields = '__all__'
