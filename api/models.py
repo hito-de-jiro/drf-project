4ad9b6176c31ddcec18789dcf31e1a5e2b381920
Hito <60074087+hito-de-jiro@users.noreply.github.com>
2024-03-21 15:50:06 +0200

    fix(model): fix unique product

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Product(models.Model):
-    product_name = models.CharField(max_length=255)
+    product_name = models.CharField(max_length=255, unique=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     customer = models.ManyToManyField(User, related_name='product_customer')
 
