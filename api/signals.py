8a70304296274342265adf3e08b02e6445ecc28f
Hito <60074087+hito-de-jiro@users.noreply.github.com>
2024-03-27 16:02:35 +0200

    feat: add update viewed lessons when product connected

--- api/signals.py
+++ api/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import m2m_changed
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from django.utils import timezone
@@ -17,12 +17,16 @@ def update_lesson_view_status(sender, instance, **kwargs):
     instance.last_watched = timezone.localtime()
 
 
-@receiver(post_save, sender=Product)
-def update_lessons(sender, instance, created, **kwargs):
-    """Update data watched lesson"""
-    if created:
-        user = instance.customer
-        lessons = instance.lesson_set.all()
-
-        for lesson in lessons:
-            LessonView.objects.get_or_create(user=user, lesson=lesson)
+@receiver(m2m_changed, sender=Product.customer.through)
+def update_lessons_on_customer_change(sender, instance, action, **kwargs):
+    if action == 'post_add' or action == 'post_remove':
+        user_ids = instance.customer.values_list('id', flat=True)
+        for user_id in user_ids:
+            for lesson in instance.product_lesson.all():
+                lesson_view, created = LessonView.objects.get_or_create(user_id=user_id, lesson=lesson)
+                if action == 'post_add':
+                    lesson_view.status_watched = False
+                    lesson_view.time_watched = 0
+                    lesson_view.save()
+                elif action == 'post_remove':
+                    lesson_view.delete()
